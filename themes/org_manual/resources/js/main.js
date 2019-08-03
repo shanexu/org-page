820d396baca7e174ea6cf087c12a48726ee7a125
Shane Xu <xusheng0711@gmail.com>
2019-08-03 18:02:39 +0800

    update

--- themes/org_manual/resources/js/main.js
+++ themes/org_manual/resources/js/main.js
@@ -1,16 +1,3 @@
-Array.prototype.forEach.call(document.getElementsByTagName('pre'), function(c){
-  if(c.classList.contains("src")) {
-    var l1 = null;
-    Array.prototype.forEach.call(c.getElementsByClassName('linenr'), function(l){
-      if(l1 == null) {
-        l.style.paddingTop = '0.5em';
-      }
-      l.innerHTML = l.innerHTML.slice(0, -2);
-      l1 = l;
-    });
-    if(l1 != null) {
-      l1.style.paddingBottom = '0.5em';
-    }
-  }
-})
-
+Array.from(document.getElementsByClassName('linenr')).forEach(function(l) {
+  l.innerHTML = l.innerHTML.slice(0, -2);
+});
